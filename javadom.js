5bf9a070975339f0a413d6e688ac500fa0ff298f
AVolosciuc <anea.tanas@mail.ru>
2022-11-10 18:40:29 +0200

    hw

--- javadom.js
+++ javadom.js
@@ -25,24 +25,24 @@
 
 // color(tag_h1)
 
-const array = [
-    "JavaScript",
-    "Dom Element",
-    "Const",
-    "let",
-    "Operator"
-]
+// const array = [
+//     "JavaScript",
+//     "Dom Element",
+//     "Const",
+//     "let",
+//     "Operator"
+// ]
 
-const array1 = [
-    "dom",
-    "Ana",
-    "Egor",
-    "JavaScript",
-    "Dom Element",
-    "Const",
-    "let",
-    "Operator"
-]
+// const array1 = [
+//     "dom",
+//     "Ana",
+//     "Egor",
+//     "JavaScript",
+//     "Dom Element",
+//     "Const",
+//     "let",
+//     "Operator"
+// ]
 
 
 // array.forEach(item =>{
@@ -52,14 +52,34 @@ const array1 = [
 //         container.appendChild(title)
 // })
 
-function createElement(params) {
-    params.forEach(item =>{
-        const title = document.createElement('p')
-        title.innerHTML = item
-        const container = document.querySelector(".container")
-        container.appendChild(title)
-})
-}
+// function createElement(params) {
+//     params.forEach(item =>{
+//         const title = document.createElement('p')
+//         title.innerHTML = item
+//         const container = document.querySelector(".container")
+//         container.appendChild(title)
+// })
+// }
 
-createElement(array)
-createElement(array1)
+// createElement(array)
+// createElement(array1)
+
+
+// HOMEWORK
+
+// document.getElementById("container_id")
+// document.getElementsByTagName("h3")
+// document.getElementsByClassName("container_class")
+
+// const doc1 = document.querySelector(".container_class")
+// doc1.innerHTML = "Hello World"
+
+// function myFunction() {
+//     document.getElementByClassName("container_class").setAttribute("class", "democlass"); 
+// }
+// myFunction()
+
+
+const body = document.querySelector(".container_class")
+const doc2 = document.createElement('h5')
+doc2.innerHTML = "Hope you're listening";
